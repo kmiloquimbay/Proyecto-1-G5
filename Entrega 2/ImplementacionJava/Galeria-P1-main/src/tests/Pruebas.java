86f524d9b77fac234daf6d538d92f2741d33a138
linamu24 <linamu24@gmail.com>
2024-05-13 20:42:05 -0500

    Pruebas

--- Entrega 2/ImplementacionJava/Galeria-P1-main/src/tests/Pruebas.java
+++ Entrega 2/ImplementacionJava/Galeria-P1-main/src/tests/Pruebas.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -24,35 +24,35 @@ import usuarios.ControladorUsuarios;
 import usuarios.OperadorSubasta;
 import usuarios.Propietario;
 
-public class Pruebas {
+class Pruebas {
     //Setup para tests de los reqs
     //Galeria
-    public static Fotografia foto1= new Fotografia("La nina y el buitre","Kevin Carter", 1993, "Sudán","20-03-2024", true, 300,  false, "10", "200");
-    public static Fotografia foto2= new Fotografia("La torre de Piza", "Anónimo", 1355, "Italia","20-12-2024", false, 500, false, "26", "300");
-    public static Video video1= new Video("La Vie", "Edith Piaf", 1983, "Francia","14-11-2024", true, false, 0,  "40", "200");
-    public static Pintura pintura1=new Pintura("La Flor", "Juan Antonio", 1964, "Italia","20-12-2025", true, true, 0,  77, 53, "Oleo");
-    public static Inventario inventario1= new Inventario();
-    public static ControladorUsuarios controlador= new ControladorUsuarios();
-    public static Galeria galeriaTests = new Galeria(inventario1,controlador);
-    public static Pintura pinturaAgregar=new Pintura("Mona Lisa", "Leonardo da Vinci", 1506, "Italia","20-10-2024", true, false, 1000, 77, 53, "Oleo");
+    static Fotografia foto1= new Fotografia("La nina y el buitre","Kevin Carter", 1993, "Sudán","20-03-2024", true, 300,  false, "10", "200");
+    static Fotografia foto2= new Fotografia("La torre de Piza", "Anónimo", 1355, "Italia","20-12-2024", false, 500, false, "26", "300");
+    static Video video1= new Video("La Vie", "Edith Piaf", 1983, "Francia","14-11-2024", true, false, 0,  "40", "200");
+    static Pintura pintura1=new Pintura("La Flor", "Juan Antonio", 1964, "Italia","20-12-2025", true, true, 0,  77, 53, "Oleo");
+    static Inventario inventario1= new Inventario();
+    static ControladorUsuarios controlador= new ControladorUsuarios();
+    static Galeria galeriaTests = new Galeria(inventario1,controlador);
+    static Pintura pinturaAgregar=new Pintura("Mona Lisa", "Leonardo da Vinci", 1506, "Italia","20-10-2024", true, false, 1000, 77, 53, "Oleo");
     
     //Usuarios
-    public static Comprador comprador= new Comprador("LuisP", "12345", "Luis Perez","3456289290", 1000000,galeriaTests.getInventario().getPiezasDisponibleVenta(), "547293");
-    public static Propietario propietario= new Propietario("LuisP", "12345", "Luis Perez", "3456289290","547293");
-    public static AdministradorGaleria admin= new AdministradorGaleria("fabio24", "1226745", "Administrador",galeriaTests, "562901");
-    public static Cajero cajero= new  Cajero("juanito2", "762598", "Cajero",galeriaTests, "4439035");
-    public static OperadorSubasta operador= new OperadorSubasta("andresP", "12235345", "Operador",galeriaTests, "653907");
+    static Comprador comprador= new Comprador("LuisP", "12345", "Luis Perez","3456289290", 1000000,galeriaTests.getInventario().getPiezasDisponibleVenta(), "547293");
+    static Propietario propietario= new Propietario("LuisP", "12345", "Luis Perez", "3456289290","547293");
+    static AdministradorGaleria admin= new AdministradorGaleria("fabio24", "1226745", "Administrador",galeriaTests, "562901");
+    static Cajero cajero= new  Cajero("juanito2", "762598", "Cajero",galeriaTests, "4439035");
+    static OperadorSubasta operador= new OperadorSubasta("andresP", "12235345", "Operador",galeriaTests, "653907");
 
     //Subastas y Compras
-    public static Oferta oferta1= new Oferta(20000, comprador);
-    public static Oferta oferta2= new Oferta(40000, comprador);
-    public static Oferta ofertaRecibir= new Oferta(25000, comprador);
-    public static Subasta subasta1= new Subasta("6748899",39034,15000,video1);
-    public static Compra compra1= new Compra("567890", 20000, "tarjeta", "La Vie","547293");
-    public static Compra compra2= new Compra("587650", 70000, "tarjeta", "La Flor","547293");
+    static Oferta oferta1= new Oferta(20000, comprador);
+    static Oferta oferta2= new Oferta(40000, comprador);
+    static Oferta ofertaRecibir= new Oferta(25000, comprador);
+    static Subasta subasta1= new Subasta("6748899",39034,15000,video1);
+    static Compra compra1= new Compra("567890", 20000, "tarjeta", "La Vie","547293");
+    static Compra compra2= new Compra("587650", 70000, "tarjeta", "La Flor","547293");
 
     
-    public static void setUp(){
+    void setUp(){
         galeriaTests.getInventario().guardarEnBodega(foto1);
         galeriaTests.getInventario().guardarEnBodega(video1);
         galeriaTests.getInventario().guardarEnBodega(pintura1);
@@ -78,116 +78,116 @@ public class Pruebas {
         galeriaTests.setAdministradorGaleria(admin);
     }
     @BeforeAll
-    public static void setUpAll(){
+    void setUpAll(){
         setUp();
     }
     
     //testUsuarios
     @Test
-    public void testAgregarComprador(){
+    void testAgregarComprador(){
         galeriaTests.getControladorUsuarios().agregarComprador(comprador);
         assertTrue(galeriaTests.getControladorUsuarios().obtenerComprador("547293").equals(comprador));
     }
     @Test
-    public void testAgregarPropietario(){
+    void testAgregarPropietario(){
         galeriaTests.getControladorUsuarios().agregarPropietario(propietario);
         assertTrue(galeriaTests.getControladorUsuarios().obtenerPropietario("547293").equals(propietario));
     }
     @Test
-    public void testAgregarEmpleado(){
+    void testAgregarEmpleado(){
         galeriaTests.getControladorUsuarios().agregarEmpleado(admin);
         assertTrue(galeriaTests.getControladorUsuarios().obtenerEmpleado("562901").equals(admin));
     }
     @Test
-    public void testAgregarSubasta(){
+    void testAgregarSubasta(){
         galeriaTests.agregarSubasta(subasta1);
         assertTrue(galeriaTests.getSubastas().containsKey("6748899"));
     }
     @Test
-    public void testAgregarCompraGaleria(){
+    void testAgregarCompraGaleria(){
         galeriaTests.agregarCompra(compra1);
         assertTrue(galeriaTests.getCompras().containsKey("567890"));
     }
     @Test
-    public void testObtenerEmpleado(){
+    void testObtenerEmpleado(){
         assertTrue(galeriaTests.getControladorUsuarios().obtenerEmpleado("562901").equals(admin));
     }
     @Test
-    public void testObtenerComprador(){
+    void testObtenerComprador(){
         assertTrue(galeriaTests.getControladorUsuarios().obtenerComprador("547293").equals(comprador));
     }
     @Test
-    public void testObtenerPropietario(){
+    void testObtenerPropietario(){
         assertTrue(galeriaTests.getControladorUsuarios().obtenerPropietario("547293").equals(propietario));
     }
     @Test
-    public void testCrearComprador(){
+    void testCrearComprador(){
         Comprador compradorNuevo= galeriaTests.getControladorUsuarios().crearComprador("Comprador1", "123", "comp","1234567890", 1000000);
         assertTrue(compradorNuevo.equals(comprador));
     }
     @Test
-    public void testCrearPropietario(){
+    void testCrearPropietario(){
         Propietario propietarioNuevo= galeriaTests.getControladorUsuarios().crearPropietario("Propietario1", "123", "prop", "1234567890");
         assertTrue(propietarioNuevo.equals(propietario));
     }
 
     //testGaleria
     @Test
-    public void testRegistrarIngresoPieza(){
+    void testRegistrarIngresoPieza(){
         admin.registrarIngresoPieza(pintura1);
         assertTrue(galeriaTests.getInventario().getPiezasEnBodega().contains(pintura1));
     }    
     @Test
-    public void testVerHistorialArtista(){
+    void testVerHistorialArtista(){
         galeriaTests.verHistorialArtista("Juan Antonio");
         assertTrue(galeriaTests.getInventario().getPiezasPasadas().contains(pintura1));
     }
     @Test
-    public void testVerHistorialPieza(){
+    void testVerHistorialPieza(){
         galeriaTests.verHistorialPieza("La Flor");
         assertTrue(galeriaTests.getCompras().containsKey("587650"));
     }
     @Test
-    public void testVerHistorialPiezaNoExiste(){
+    void testVerHistorialPiezaNoExiste(){
         galeriaTests.verHistorialPieza("La Flor2");
         assertTrue(galeriaTests.getCompras().containsKey("587650"));
     }
     @Test
-    public void testVerHistorialArtistaNoExiste(){
+    void testVerHistorialArtistaNoExiste(){
         galeriaTests.verHistorialArtista("Juan Antonio2");
         assertTrue(galeriaTests.getInventario().getPiezasPasadas().contains(pintura1));
     }
     @Test
-    public void testEncontarCompra(){
+    void testEncontarCompra(){
         assertTrue(galeriaTests.encontrarCompra("587650").equals(compra2));
     }
 
     //testPieza
     @Test
-    public void testAgregarPieza(){
+    void testAgregarPieza(){
         propietario.agregarPieza(pinturaAgregar);
         assertTrue(propietario.getMisPiezasActuales().contains(pinturaAgregar));
     }
     @Test
-    public void testPasarAPasadas(){
+    void testPasarAPasadas(){
         propietario.pasarAPasadas(pinturaAgregar);
         assertTrue(propietario.getMisPiezasActuales().contains(pinturaAgregar));
     }
 
     //testSubasta
     @Test
-    public void testAgregarOferta(){
+    void testAgregarOferta(){
         subasta1.agregarOferta(ofertaRecibir);
         assertTrue(subasta1.getOfertas().contains(ofertaRecibir));
     }
 
     //testCompra
     @Test
-    public void testVerificarVentaValorFijo(){
+    void testVerificarVentaValorFijo(){
         assertTrue(compra1.verificarVentaValorFijo(foto1, 20000));
     }
     @Test
-    public void testVerificarVentaValorFijoNoCumple(){
+    void testVerificarVentaValorFijoNoCumple(){
         assertTrue(!compra1.verificarVentaValorFijo(foto1, 10000));
     }
     
@@ -195,12 +195,12 @@ public class Pruebas {
     //testAdmin
 
     @Test
-    public void testRegistrarIngresoPiezaAdmin(){
+    void testRegistrarIngresoPiezaAdmin(){
         admin.registrarIngresoPieza(pintura1);
         assertTrue(galeriaTests.getInventario().getPiezasEnBodega().contains(pintura1));
     }
     @Test
-    public void testConfirmarVentaAdmin_Integracion(){
+    void testConfirmarVentaAdmin_Integracion(){
         admin.confirmarVenta(compra1, foto1, "547293");
         assertEquals("Venta confirmada", admin.confirmarVenta(compra1, foto1, "547293"));
         assertTrue(galeriaTests.getInventario().getPiezasPasadas().contains(foto1));
@@ -209,42 +209,42 @@ public class Pruebas {
         
     }
     @Test
-    public void testVerificarComprador(){
+    void testVerificarComprador(){
         assertTrue(admin.verificarComprador("547293"));
 
     }
 
     @Test
-    public void testDevolucionPiezaAdmin_Integracion(){
+    void testDevolucionPiezaAdmin_Integracion(){
         admin.devolucionPieza(pintura1, "547293");
         assertTrue(galeriaTests.getInventario().getPiezasEnBodega().contains(pintura1));
         assertTrue(!galeriaTests.getControladorUsuarios().obtenerPropietario("547293").getMisPiezasActuales().contains(pintura1));
     }
     @Test
-    public void testBloquearPieza(){
+    void testBloquearPieza(){
         admin.bloquearPieza("La Flor");
         assertTrue(pintura1.isBloqueada());
     }
     @Test
-    public void testDesbloquearPieza(){
+    void testDesbloquearPieza(){
         admin.desbloquearPieza("La Flor");
         assertTrue(!pintura1.isBloqueada());
     }
     @Test
-    public void testAumentarLimite(){
+    void testAumentarLimite(){
         admin.aumentarLimite("547293", 1000000);
         assertTrue(comprador.getLimiteCompras()==2000000);
     }
     
     //testCajero
     @Test
-    public void testRegistrarCompra(){
+    void testRegistrarCompra(){
         admin.confirmarVenta(compra1, foto1, "547293");
         assertEquals("Pago registrado", cajero.registrarPago(compra1, foto1, "547293"));
     }
 
     @Test
-    public void testEntregarPieza_Integracion(){
+    void testEntregarPieza_Integracion(){
         admin.confirmarVenta(compra1, foto1, "547293");
         assertEquals("547293", cajero.entregarPieza(foto1, "547293"));
         assertTrue(galeriaTests.getInventario().getPiezasEnExhibicion().contains(foto1));
@@ -254,46 +254,46 @@ public class Pruebas {
     
     //testOperador
     @Test
-    public void testRecibirRegistrarOferta(){
+    void testRecibirRegistrarOferta(){
         assertTrue(operador.recibirRegistrarOferta(ofertaRecibir, "6748899").equals("Oferta registrada"));
 
     }
     @Test
-    public void testEvaluarOferta(){
+    void testEvaluarOferta(){
         assertTrue(operador.evaluarOferta(ofertaRecibir, "6748899"));
     }
     @Test
-    public void testTerminarSubasta(){
+    void testTerminarSubasta(){
         assertTrue(operador.terminarSubasta("6748899").equals("Subasta terminada"));
     }
     @Test
-    public void testTerminarSubastaNoExiste(){
+    void testTerminarSubastaNoExiste(){
         assertTrue(operador.terminarSubasta("6748899").equals("Subasta terminada"));
     }
 
     //testComprador
     @Test
-    public void testRealizarCompraFija(){
+    void testRealizarCompraFija(){
         assertEquals("Compra realizada", comprador.realizarCompraFija(foto1));
     }
     @Test
-    public void testVerHistorialCompras(){
+    void testVerHistorialCompras(){
         comprador.verHistorialCompras();
         assertTrue(comprador.getmisCompras().contains(compra1));
     }
     @Test
-    public void testVerHistorialComprasVacio(){
+    void testVerHistorialComprasVacio(){
         comprador.getmisCompras().clear();
         comprador.verHistorialCompras();
         assertTrue(comprador.getmisCompras().isEmpty());
     }
     @Test
-    public void testAgregarCompra(){
+    void testAgregarCompra(){
         comprador.agregarCompra(compra1);
         assertTrue(comprador.getmisCompras().contains(compra1));
     }
     @Test
-    public void testAgregarCompraVacia(){
+    void testAgregarCompraVacia(){
         comprador.getmisCompras().clear();
         comprador.agregarCompra(compra1);
         assertTrue(comprador.getmisCompras().contains(compra1));
@@ -302,53 +302,53 @@ public class Pruebas {
     //testPropietario
 
     @Test
-    public void testPasarAPasadasPropietario(){
+    void testPasarAPasadasPropietario(){
         propietario.pasarAPasadas(pintura1);
         assertTrue(propietario.getMisPiezasActuales().contains(pintura1));
     }
     @Test
-    public void testAgregarAPasadas(){
+    void testAgregarAPasadas(){
         propietario.agregarAPasadas(pintura1);
         assertTrue(propietario.getMisPiezasPasadas().contains(pintura1));
     }
     @Test
-    public void testAgregarPiezaPropietario(){
+    void testAgregarPiezaPropietario(){
         propietario.agregarPieza(pinturaAgregar);
         assertTrue(propietario.getMisPiezasActuales().contains(pinturaAgregar));
     }
 
     //testInventario
     @Test
-    public void testGuardarEnBodega(){
+    void testGuardarEnBodega(){
         galeriaTests.getInventario().guardarEnBodega(foto2);
         assertTrue(galeriaTests.getInventario().getPiezasEnBodega().contains(foto2));
     }
     @Test
-    public void testPonerEnDisponibles(){
+    void testPonerEnDisponibles(){
         galeriaTests.getInventario().ponerEnDisponibles(foto2);
         assertTrue(galeriaTests.getInventario().getPiezasDisponibleVenta().contains(foto2));
     }
     @Test
-    public void testPonerEnExhibicion(){
+    void testPonerEnExhibicion(){
         galeriaTests.getInventario().pasarAExhibicion(foto2);
         assertTrue(galeriaTests.getInventario().getPiezasEnExhibicion().contains(foto2));
     }
     @Test
-    public void testBuscarPiezaEnDisponibles(){
+    void testBuscarPiezaEnDisponibles(){
         assertTrue(galeriaTests.getInventario().buscarPieza("La nina y el buitre").equals(foto1));
     }
 
     @Test
-    public void testBuscarPiezaEnBodega(){
+    void testBuscarPiezaEnBodega(){
         assertTrue(galeriaTests.getInventario().buscarPieza("La torre de Piza").equals(foto2));
     }
     @Test
-    public void testBuscarPiezaEnPasadas(){
+    void testBuscarPiezaEnPasadas(){
         assertTrue(galeriaTests.getInventario().buscarPieza("La Flor").equals(pintura1));
     }
 
     @AfterAll   
-    public static void setDown(){
+    static void setDown(){
         galeriaTests.getInventario().getPiezasEnBodega().clear();
         galeriaTests.getInventario().getPiezasEnExhibicion().clear();
         galeriaTests.getInventario().getPiezasDisponibleVenta().clear();
